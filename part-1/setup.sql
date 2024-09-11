bb834a9ba120f554e9f843b7c1ba40ae513e8645
MitchLloyd <mitchell@ada.ac.uk>
2024-09-11 07:53:55 +0000

    remove insertion setup from part 1

--- part-1/setup.sql
+++ part-1/setup.sql
@@ -1,10 +1,5 @@
-.mode csv
 .mode columns
 .open test_todos.sqlite
 
 -- Create the users and todos table here
-
-
--- Check you can import sample data into the tables you've created to verify your SQL statements are working
--- .import ./users.csv users
--- .import todos.csv todos
+-- Insert users and todos data to check your tables are defined correctly
